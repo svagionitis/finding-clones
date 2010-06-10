d7b5eec2d40b9edb6266c27777e5142311d7ff0f
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2010-06-10 20:38:50 +0100

    modified:   alg_lvl1.c 	modified:   alg_lvl1.h 	modified:   alg_lvl2.c 	modified:   alg_lvl2.h 	modified:   alg_lvl3.c 	modified:   alg_lvl3.h

---
+++ alg_lvl1.h
@@ -0,0 +1,20 @@
+/* ############################################################################
+Name           : alg_lvl1.h
+Company        : S. Vagionitis
+Project        : Finding Clones
+Programmer     : S. Vagionitis
+Revisor        : S. Vagionitis
+Description    : Header file for alg_lvl1.c.
+
+
+Programmer     Date           Action
+============== ============== =================================================
+S. Vagionitis  10/06/2010     Creation
+
+############################################################################ */
+#ifndef __ALG_LVL1_H__
+#define __ALG_LVL1_H__
+
+
+#endif
+
---
+++ alg_lvl3.c
@@ -0,0 +1,24 @@
+/* ############################################################################
+Name           : alg_lvl3.c
+Company        : S. Vagionitis
+Project        : Finding Clones
+Programmer     : S. Vagionitis
+Revisor        : S. Vagionitis
+Description    : Functions for Algorithm Level 3.
+
+Procedure                     Description
+============================= =================================================
+
+
+Globals        Type           Description
+============== ============== =================================================
+
+
+Programmer     Date           Action
+============== ============== =================================================
+S. Vagionitis  10/06/2010     Creation
+
+############################################################################ */
+
+#define __ALG_LVL3_C__
+
