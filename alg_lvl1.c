db2a6aa12102a1cbaf2a157fbca71a622e06731c
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2010-06-11 14:21:11 +0100

    modified:   alg_lvl1.c 	modified:   alg_lvl1.h

--- alg_lvl1.c
+++ alg_lvl1.c
@@ -21,4 +21,78 @@ S. Vagionitis  10/06/2010     Creation
 ############################################################################ */
 
 #define __ALG_LVL1_C__
+#include "alg_lvl1.h"
 
+/* ############################################################################
+Name           : create_sub_images
+Description    : 
+
+
+Arguments          Type               Description
+================== ================== =========================================
+image_data(IN)     unsigned char *    Image data.
+width(IN)          int                Width of image.
+height(IN)         int                Height of image.
+shift_step(IN)     unsigned int       Number of pixels to shift the 
+                                      M x M window.
+subimage_data(OUT) unsigned char **** Subimages data.
+
+Return Values                     Description
+===================================== =========================================
+
+Globals            Type           Description
+================== ============== =============================================
+
+Locals             Type           Description
+================== ============== =============================================
+i, j, k, l         unsigned int
+steps              unsigned int
+width_subimages    unsigned int
+height_subimages   unsigned int
+
+############################################################################ */
+int create_sub_images(unsigned char *image_data, int width, int height, unsigned int shift_step, unsigned char ****subimage_data)
+{
+unsigned int i = 0, j = 0, k = 0, l = 0;
+
+unsigned int steps = M / shift_step;
+unsigned int width_subimages = steps * (width / M);
+unsigned int height_subimages = steps * (height / M);
+
+/* Allocate memory for subimge data*/
+subimage_data = (unsigned char ****)malloc(width_subimages * sizeof(unsigned char ***));
+if (subimage_data == NULL){
+	printf("Could not allocate %d bytes.\n", (width_subimages * sizeof(unsigned char ***)));
+	return FALSE;
+	}
+else{
+	for (i = 0;i<width_subimages;i++){
+		subimage_data[i] = (unsigned char ***)malloc(height_subimages * sizeof(unsigned char **));
+		if (subimage_data[i] == NULL){
+			printf("Could not allocate %d bytes for i=%d index.\n", (height_subimages * sizeof(unsigned char **)), i);
+			return FALSE;
+			}
+		else{
+			for (j=0;j<height_subimages;j++){
+				subimage_data[i][j] = (unsigned char **)malloc(M * sizeof(unsigned char *));
+				if (subimage_data[i][j] == NULL){
+					printf("Could not allocate %d bytes for j=%d index.\n", (M * sizeof(unsigned char *)), j);
+					return FALSE;
+					}
+				else{
+					for(k=0;k<M;k++){
+						subimage_data[i][j][k] = (unsigned char *)malloc(M * sizeof(unsigned char));
+						if (subimage_data[i][j][k] == NULL){
+							printf("Could not allocate %d bytes for k=%d index.\n", (M * sizeof(unsigned char)), k);
+							return FALSE;
+							}
+						}//for k
+					}//else subimage_data[i][j]
+				}//for j
+			}//else subimage_data[i]
+		}//for i
+	}//else subimage_data
+
+
+
+}
--- alg_lvl1.h
+++ alg_lvl1.h
@@ -14,6 +14,14 @@ S. Vagionitis  10/06/2010     Creation
 ############################################################################ */
 #ifndef __ALG_LVL1_H__
 #define __ALG_LVL1_H__
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * M x M regions that image will be divided. 
+ * We could use also 24, 18 and 12 as stated in the paper.
+ */
+#define M 32
 
 
 #endif
